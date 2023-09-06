516f0ad20130290603a973dfc99ce751cf399dc3
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2023-09-06 10:46:48 -0300

    commit message

--- aplic/urls.py
+++ aplic/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("index.html", views.IndexView.as_view(), name="index"),
+    path("contatos.html", views.ContatoView.as_view(), name="index"),
 ]
--- aplic/views.py
+++ aplic/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ContatoView(TemplateView):
+    template_name = 'contatos.html'
